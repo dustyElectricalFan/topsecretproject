8fca5b612ac04571c02e39dd492f9593706c3f45
dustyElectricalFan <33040779+dustyElectricalFan@users.noreply.github.com>
2021-10-26 23:58:16 -0400

    Add files via upload

---
+++ topsecret1026/Assets/Scripts/logManipulator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class logManipulator : MonoBehaviour
+{
+    private static logManipulator instance;
+    private Text dialouge;
+
+    private void Awake(){
+        instance = this;
+        dialouge = transform.Find("logDisplayBackground/logDisplayText").GetComponent<Text>();
+        dialouge.text = "";
+        
+    }
+
+   public static void displayLogStatic (List<List<string>> displayText)
+   {
+       instance.displayLog(displayText);
+   }
+
+   public static void pullUpDisplayStatic ()
+   {
+       instance.gameObject.SetActive(true);
+   }
+
+   private void displayLog (List<List<string>> displayText)
+   {
+      
+      foreach(List<string> bext in displayText)
+      {
+          dialouge.text += bext[0];
+          dialouge.text += '\n';
+          dialouge.text += bext[1];
+          dialouge.text += '\n';
+          dialouge.text += '\n';
+      }
+   }
+
+   public void endDisplay()
+   {
+       instance.gameObject.SetActive(false);
+   }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ topsecret1026/Assets/Scripts/spriteManipulator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spriteManipulator : MonoBehaviour
+{
+    private GameObject sprite1;
+    private GameObject sprite2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void enableSprite(string spritenum)
+    {
+     
+    }
+}
